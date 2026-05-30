d9b622a58a1cee665b48d9fd49f4f187209ccec3
Oscar Ruiz <oscarruiz2614@gmail.com>
2026-05-30 10:51:04 -0500

    Add AppTheme class and integrate it into main application theme

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+const List<Color> colorList = [
+  Colors.red,
+  Colors.pink,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.indigo,
+  Colors.blue,
+  Colors.lightBlue,
+  Colors.cyan,
+  Colors.teal,
+  Colors.green,
+  Colors.lightGreen,
+  Colors.lime,
+  Colors.yellow,
+  Colors.amber,
+  Colors.orange,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+    : assert(selectedColor >= 0, 'Selected color index must be less than 0'),
+      assert(
+        selectedColor < colorList.length,
+        'Selected color index must be less than ${colorList.length}',
+      );
+
+  ThemeData getTheme() =>
+      ThemeData(useMaterial3: true, colorSchemeSeed: colorList[selectedColor]);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,12 +11,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 11).getTheme(),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Material App Bar'),
+          title: Text('Material App Bar'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Center(
+          child: FilledButton(onPressed: () {}, child: Text('Hello World')),
         ),
       ),
     );
