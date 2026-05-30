8d2c2c4eb0e359c79fe5116b99e08f8900dcea30
Oscar Ruiz <oscarruiz2614@gmail.com>
2026-05-30 15:54:04 -0500

    Add CustomButton widget to ButtonsScreen for enhanced button functionality

--- lib/presentation/screens/buttons/buttons_screnn.dart
+++ lib/presentation/screens/buttons/buttons_screnn.dart
@@ -64,6 +64,8 @@ class _ButtonsView extends StatelessWidget {
               icon: const Icon(Icons.adb_outlined),
             ),
 
+            CustomButton(),
+
             IconButton(
               onPressed: () {},
               icon: const Icon(Icons.app_registration_outlined),
@@ -77,6 +79,29 @@ class _ButtonsView extends StatelessWidget {
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text('Hola mundo', style: TextStyle(color: Colors.white)),
+          ),
         ),
       ),
     );
